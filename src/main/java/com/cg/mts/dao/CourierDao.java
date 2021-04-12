3fef7eec81024b34bb250ad9d9d705050b524dca
Tarun-Kumar-stk <tarunstk8897@gmail.com>
2021-04-12 10:28:57 +0530

    push

--- src/main/java/com/cg/mts/dao/CourierDao.java
+++ src/main/java/com/cg/mts/dao/CourierDao.java
@@ -8,48 +8,50 @@ import org.springframework.stereotype.Component;
 
 import com.cg.mts.entities.Courier;
 import com.cg.mts.entities.CourierStatus;
-import com.cg.mts.exceptions.CourierIdExistsException;
-import com.cg.mts.exceptions.CourierNotFoundException;
-import com.cg.mts.repository.CourierRepository;
+import com.cg.mts.exception.CourierNotFoundException;
+import com.cg.mts.repositories.CourierRepository;
 
 @Component
-public class CourierDao implements ICourierDao{
+public class CourierDao implements ICourierDao {
 
 	@Autowired
 	CourierRepository courRepository;
 
+	@Override
 	public List<Courier> getAllCouriers() {
-			List<Courier> cour = courRepository.findAll();
-			return cour;
+		List<Courier> cour = courRepository.findAll();
+		return cour;
 	}
 
-	public Courier getCourier(int courierId) throws CourierNotFoundException {
-		if (!(courRepository.existsById(courierId))) {
-			throw new CourierNotFoundException("Courier with Id " + courierId + " not found");
-		} 
-		else {
-			Optional<Courier> courier1 = courRepository.findById(courierId);
-			return courier1.get();
-		}
+	@Override
+	public void addCourier(Courier courier) {
+		if(!(courRepository.existsById(courier.getCourierId())))
+				courRepository.save(courier);	
 	}
-
-	public void addCourier(Courier courier) throws CourierIdExistsException{
-		if(courRepository.existsById(courier.getCourierId())) {
-			courRepository.save(courier);
-		}
-		throw new CourierIdExistsException("Courier with id" + courier.getCourierId() + " already exists");
+	
+	@Override
+	public Courier getCourier(int courierId) throws CourierNotFoundException  {
+		 if (!(courRepository.existsById(courierId))) {
+		 throw new CourierNotFoundException("Courier with Id " + courierId + " not found");
+		 } else {
+				Optional<Courier> courier1 = courRepository.findById(courierId);
+				return courier1.get();
+		 }
 	}
+	
 
+	@Override
 	public boolean deleteCourier(int courierId) {
-		if(courRepository.existsById(courierId)) {
+		if (courRepository.existsById(courierId)) {
 			courRepository.deleteById(courierId);
 			return true;
 		}
 		return false;
 	}
-	
+
+	@Override
 	public boolean updateCourier(Courier courier) {
-		if(courRepository.existsById(courier.getCourierId())) {
+		if (courRepository.existsById(courier.getCourierId())) {
 			courRepository.save(courier);
 			return true;
 		}
@@ -57,10 +59,15 @@ public class CourierDao implements ICourierDao{
 	}
 
 	@Override
-	public CourierStatus getCourierStatus(Courier courier) throws CourierNotFoundException{
-		if(courRepository.existsById(courier.getCourierId())) {
-			return courier.getStatus();
-		}
-		throw new CourierNotFoundException("Courier does not exists");
+	public CourierStatus getCourierStatus(Courier courier) {
+		Optional<Courier> courier1 = courRepository.findById(courier.getCourierId());
+		return courier1.get().getStatus();
+	}
+
+	@Override
+	public CourierStatus updateCourierStatus(Courier courier) {
+		Optional<Courier> courier1 = courRepository.findById(courier.getCourierId());
+		courier1.get().setStatus(CourierStatus.delivered);
+		return courier1.get().getStatus();
 	}
 }
--- src/main/java/com/cg/mts/dao/ICourierDao.java
+++ src/main/java/com/cg/mts/dao/ICourierDao.java
@@ -4,14 +4,21 @@ import java.util.List;
 
 import com.cg.mts.entities.Courier;
 import com.cg.mts.entities.CourierStatus;
-import com.cg.mts.exceptions.CourierIdExistsException;
-import com.cg.mts.exceptions.CourierNotFoundException;
+import com.cg.mts.exception.CourierNotFoundException;
 
 public interface ICourierDao {
-	public List<Courier> getAllCouriers();
-	public Courier getCourier(int courierId) throws CourierNotFoundException;
-	public void addCourier(Courier courier) throws CourierIdExistsException;
-	CourierStatus getCourierStatus(Courier courier) throws CourierNotFoundException;
-	
 
+	List<Courier> getAllCouriers();
+	
+	public Courier getCourier(int courierId) throws CourierNotFoundException;
+	
+	public void addCourier(Courier courier);
+	
+	boolean deleteCourier(int courierId);
+	
+	boolean updateCourier(Courier courier);
+	
+	CourierStatus getCourierStatus(Courier courier);
+	
+	CourierStatus updateCourierStatus(Courier courier);
 }
